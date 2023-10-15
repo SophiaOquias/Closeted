281762d99040c899446cf3859310a8a1f4a68423
Sophia <sophia.oquias@gmail.com>
2023-10-15 21:09:23 +0800

    created bottom nav bar

--- app/src/main/java/com/closeted/MainActivity.kt
+++ app/src/main/java/com/closeted/MainActivity.kt
@@ -2,10 +2,45 @@ package com.closeted
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.closeted.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(ClosetFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+
+            when(item.itemId) {
+                R.id.navCloset -> {
+                    replaceFragment(ClosetFragment())
+                }
+                R.id.navOutfits -> {
+                    replaceFragment(OutfitsFragment())
+                }
+                R.id.navAdd -> {
+                    replaceFragment(AddFragment())
+                }
+                R.id.navLaundry -> {
+                    replaceFragment(LaundryFragment())
+                }
+            }
+
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame, fragment)
+        fragmentTransaction.commit()
     }
 }
