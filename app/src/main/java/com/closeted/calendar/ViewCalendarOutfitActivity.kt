b88ba048e2f1f76fbbdd31130e3f1834a89aaa7f
Sophia <sophia.oquias@gmail.com>
2023-11-16 20:21:45 +0800

    renamed layout file

--- app/src/main/java/com/closeted/calendar/ViewCalendarOutfitActivity.kt
+++ app/src/main/java/com/closeted/calendar/ViewCalendarOutfitActivity.kt
@@ -23,7 +23,7 @@ class ViewCalendarOutfitActivity : AppCompatActivity() {
     private lateinit var adapter: ViewOutfitAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.calendar_view_outfit)
+        setContentView(R.layout.activity_calendar_view_outfit)
 
         val imgList: ArrayList<Int> = ArrayList()
 
